1ebcc46c4eb90d33b44223ef4bb2c5e4c8cd291c
Sufyan Adam <pair+sufyanadam@gmail.com>
2018-09-21 17:28:50 -0700

    Backup emacs dir with timestamp

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ cd ~
 brew update
 brew install emacs --with-cocoa
 brew install cask
+brew install mplayer
 if [ -d ~/.emacs.d ]; then
   echo backing up existing emacs dir
-  mv ~/.emacs.d ~/.emacs.d.bak
+  mv ~/.emacs.d ~/.emacs.d.bak-$(date -u +'%Y%m%d%H%M%S')
 fi
 git clone https://github.com/sufyanadam/.emacs.d.git
 cd ~/.emacs.d
