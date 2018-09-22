14eef564e114e9cfcd812ee4f88fdeed3a57e9f2
Sufyan Adam <pair+sufyanadam@gmail.com>
2018-09-21 17:47:59 -0700

    Install hack font

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ brew update
 brew install emacs --with-cocoa
 brew install cask
 brew install mplayer
+brew tap caskroom/fonts
+brew cask install font-hack
+
 if [ -d ~/.emacs.d ]; then
   echo backing up existing emacs dir
   mv ~/.emacs.d ~/.emacs.d.bak-$(date -u +'%Y%m%d%H%M%S')
